2109a074557766318baa335112ac10a192d3ebb0
siso25 <siso25@zoho.com>
2023-09-05 22:05:53 +0900

    postcssとdaisyUIを導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
+    './app/views/**/*.html.slim',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
+  ],
+  plugins: [
+    require('daisyui')
   ]
 }
