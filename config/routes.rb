12f3067ae04fa7513f326796aeb5e1cc91ec9083
siso25 <siba2150@outlook.jp>
2023-09-18 14:23:22 +0900

    新規ルーレットページの作成機能を追加
    トップページのボタンを押して新規ルーレットページ開くように変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root "home#index"
+  resources :roulettes, only: %i[show create]
 end
