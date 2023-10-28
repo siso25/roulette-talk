e3c994d5e48ba5c48e0bd706f74c0246b7ef35c1
siso25 <siso25@zoho.com>
2023-10-28 22:48:43 +0900

    トークテーマの作成、編集フォームを追加
    トークテーマの新規作成、編集用のフォームを追加。
    表示の切り替えにTurbo Framesを使用するようにした。

--- app/controllers/talk_themes_controller.rb
+++ app/controllers/talk_themes_controller.rb
@@ -1,2 +1,11 @@
 class TalkThemesController < ApplicationController
+  def new
+    @roulette = Roulette.find(params[:roulette_id])
+    @talk_theme = TalkTheme.new
+  end
+
+  def edit
+    @roulette = Roulette.find(params[:roulette_id])
+    @talk_theme = @roulette.talk_themes.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@
 
 Rails.application.routes.draw do
   root "home#index"
-  resources :roulettes, only: %i[show create]
+  resources :roulettes, only: %i[show create] do
+    resources :talk_themes, only: %i[new create edit update]
+  end
 end
