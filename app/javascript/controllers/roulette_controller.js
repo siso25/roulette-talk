673f2a168d48e58627cbc9355725bcf423d63be8
siso25 <siso25@zoho.com>
2023-11-19 22:00:31 +0900

    セッションストレージの内容初期化機能追加
    トークテーマと話す人の登録内容によってセッションストレージの
    内容が書き換わるように修正

--- app/javascript/controllers/roulette_controller.js
+++ app/javascript/controllers/roulette_controller.js
@@ -2,13 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 import { saveTargets } from "../helpers/storage"
 
 export default class extends Controller {
+  static targets = ['talkTheme', 'speakerName']
+
   connect() {
-    saveTargets('talk', '1, 2, 3, 4')
-    saveTargets('speaker', '1, 2, 3, 4')
+    this.reset()
   }
 
   reset() {
-    saveTargets('talk', '1, 2, 3, 4')
-    saveTargets('speaker', '1, 2, 3, 4')
+    saveTargets('talk', this.createLotteryString(this.talkThemeTargets.length))
+    saveTargets('speaker', this.createLotteryString(this.speakerNameTargets.length))
+  }
+
+  private
+
+  createLotteryString(element_count) {
+    let numbers = []
+    for (let i = 0; i < element_count; i++) {
+      numbers.push(i)
+    }
+
+    return numbers.join()
   }
 }
