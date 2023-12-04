a19937200cbce07bc7e90e3470a09ecea57d309d
siso25 <siso25@zoho.com>
2023-12-04 22:17:21 +0900

    リンクコピー時にツールチップを表示するよう変更

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,7 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="clipboard"
 export default class extends Controller {
+  static targets = ['toolTip']
+
   copy() {
     navigator.clipboard.writeText(location.href)
+    this.displayToolTip()
+  }
+
+  displayToolTip() {
+    this.toolTipTarget.classList.add('tooltip', 'tooltip-open', 'tooltip-success')
+    const promise = new Promise(resolve => setTimeout(resolve, 2000))
+    promise.then(() => this.toolTipTarget.classList.remove('tooltip', 'tooltip-open', 'tooltip-success'))
   }
 }
