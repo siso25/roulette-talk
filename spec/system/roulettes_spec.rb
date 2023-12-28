55679d8c685de355dd4695b75a01218db1f15776
siso25 <siso25@zoho.com>
2023-12-28 20:41:10 +0900

    使い方ボタンとURLコピーボタンのテスト追加
    使い方ボタンとURLコピーボタンのテストを追加した。
    また、トップページのテストに関しては別ファイルに書くように変更。

--- spec/system/roulettes_spec.rb
+++ spec/system/roulettes_spec.rb
@@ -1,30 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "Roulettes", type: :system do
-  scenario "user creates a new roulette" do
-    visit root_path
-    expect do
-      click_link "ルーレットを作る"
-    end.to change(Roulette.all, :count).by(1)
-    expect(page).to have_current_path %r{^/roulettes/[^/]+$}
-  end
-
-  scenario "user opens term of service", js: true do
-    visit root_path
-    click_button '利用規約'
-    expect(page).to have_css "dialog#term_of_service_modal"
-  end
-
-  scenario "user opens privacy policy", js: true do
-    visit root_path
-    click_button 'プライバシーポリシー'
-    expect(page).to have_css "dialog#privacy_policy_modal"
+  before do
+    @roulette = Roulette.create
+    FactoryBot.create_list(:talk_theme, 4, roulette: @roulette)
+    FactoryBot.create_list(:speaker, 4, roulette: @roulette)
   end
 
   scenario "user opens help modal", js: true do
-    visit root_path
-    click_link "ルーレットを作る"
+    visit roulette_path(@roulette)
     click_button '使い方'
     expect(page).to have_css "dialog#help_modal"
+  end
+
+  scenario "user copies link on current page", js: true do
+    visit roulette_path(@roulette)
+    click_button "リンクのコピー"
+    cdp_permission = {
+      origin: page.server_url,
+      permission: { name: 'clipboard-read' },
+      setting: 'granted'
+    }
+    page.driver.browser.execute_cdp('Browser.setPermission', **cdp_permission)
+    copied_url = evaluate_async_script('navigator.clipboard.readText().then(arguments[0])')
+    current_url = evaluate_script('location.href')
+    expect(copied_url).to eq current_url
+  end
+
+  scenario 'it displays result stored in session storage', js: true do
+    visit roulette_path(@roulette)
+    click_button 'スタート'
+    result_text = find "[data-rotate-target='resultText']", wait: 10
+    session_storage_items = JSON.parse(evaluate_script("sessionStorage.getItem('#{@roulette.id}')"))
+    expect(result_text).to have_content(session_storage_items['talkResult'])
+    expect(result_text).to have_content(session_storage_items['speakerResult'])
   end
 end
