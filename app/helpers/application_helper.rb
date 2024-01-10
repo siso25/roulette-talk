06f7b730e7f047b2997a24a00e96fc89bec233cf
siso25 <siso25@zoho.com>
2024-01-10 20:17:24 +0900

    meta titleとmeta descriptionを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'RouletteTalk',
+      description: 'トークテーマと話す人を設定したルーレットを保存できます。オンライン飲み会やイベント等でお使いください。'
+    }
+  end
 end
---
+++ config/initializers/meta_tags.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+MetaTags.configure do |config|
+end
