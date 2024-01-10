1988759dc4b5dc7dbfd2ac1bce1e1d3c09382810
siso25 <siso25@zoho.com>
2024-01-10 21:48:35 +0900

    OGPとTwitterCardを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,20 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'RouletteTalk',
-      description: 'トークテーマと話す人を設定したルーレットを保存できます。オンライン飲み会やイベント等でお使いください。'
+      charset: 'utf-8',
+      description: 'トークテーマと話す人を設定したルーレットを保存できます。オンライン飲み会やイベント等でお使いください。',
+      icon: { href: image_url('favicon.ico') },
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png')
+      },
+      twitter: {
+        card: 'summary_large_image'
+      }
     }
   end
 end
