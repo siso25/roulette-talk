9a042f3cb7f1c5b2e478134ddc124a88845e950e
siso25 <siso25@zoho.com>
2024-01-10 21:58:59 +0900

    helperにviewportの記述を移動

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,7 @@ module ApplicationHelper
       site: 'RouletteTalk',
       charset: 'utf-8',
       description: 'トークテーマと話す人を設定したルーレットを保存できます。オンライン飲み会やイベント等でお使いください。',
+      viewport: 'width=device-width,initial-scale=1',
       icon: { href: image_url('favicon.ico') },
       og: {
         site_name: :site,
