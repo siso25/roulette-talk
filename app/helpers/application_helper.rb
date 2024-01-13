cdf99e95ce71a447b997a31f04572f92210c1089
siso25 <siso25@zoho.com>
2024-01-13 15:14:04 +0900

    各ページにtitleとdescriptionを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,10 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'RouletteTalk',
+      title: 'トークテーマルーレット作成ツール',
+      reverse: true,
       charset: 'utf-8',
-      description: 'トークテーマと話す人を設定したルーレットを保存できます。オンライン飲み会やイベント等でお使いください。',
+      description: 'トークテーマと話す人を設定したルーレットを作成・保存できます。オンライン飲み会やイベント等でお使いください。',
       viewport: 'width=device-width,initial-scale=1',
       icon: { href: image_url('favicon.ico') },
       og: {
