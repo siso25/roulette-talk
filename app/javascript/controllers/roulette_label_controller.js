dc87e74e9f9b70a07773247d75298a2fc42f12f5
siso25 <siso25@zoho.com>
2024-01-15 00:23:33 +0900

    ラベル表示位置をレスポンシブ対応した

--- app/javascript/controllers/roulette_label_controller.js
+++ app/javascript/controllers/roulette_label_controller.js
@@ -6,25 +6,48 @@ export default class extends Controller {
     count: Number,
     radius: Number,
     topPlus: Number,
-    leftPlus: Number
+    leftPlus: Number,
+    mdRadius: Number,
+    mdTopPlus: Number,
+    mdLeftPlus: Number,
+    lgRadius: Number,
+    lgTopPlus: Number,
+    lgLeftPlus: Number
   }
 
   connect() {
+    this.setLabelPosition()
+  }
+
+  setLabelPosition() {
+    const obj = this.switchByWidth()
+    console.log(obj)
     const angle = 360 / this.countValue
     const itemPositionAngle = angle / 2 + angle * this.indexValue
     const position = this.calcTopAndLeftPosition(
       this.element,
       itemPositionAngle,
-      this.radiusValue,
-      this.leftPlusValue,
-      this.topPlusValue
+      obj.radius,
+      obj.leftPlus,
+      obj.topPlus
     )
     const rotateAngle = this.calcRotateAngle(itemPositionAngle)
-
     this.element.style = `left: ${position[0]}px; top: ${position[1]}px; transform: rotate(${rotateAngle}deg);`
   }
 
   private
+
+  switchByWidth() {
+    if (window.matchMedia('(min-width: 1096px)').matches) {
+      return { radius: this.lgRadiusValue, topPlus: this.lgTopPlusValue, leftPlus: this.lgLeftPlusValue }
+    }
+
+    if (window.matchMedia('(min-width: 768px)').matches) {
+      return { radius: this.mdRadiusValue, topPlus: this.mdTopPlusValue, leftPlus: this.mdLeftPlusValue }
+    }
+
+    return { radius: this.radiusValue, topPlus: this.topPlusValue, leftPlus: this.leftPlusValue }
+  }
 
   calcTopAndLeftPosition(element, angle, radius, leftPlus, topPlus) {
     const radian = (this.calcRadian(angle) * Math.PI) / 180
