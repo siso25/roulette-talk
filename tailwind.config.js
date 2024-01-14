b99e214e016a73d2c42493f3298374e935c4be0c
siso25 <siso25@zoho.com>
2024-01-14 21:17:50 +0900

    ルーレットページを縦の配置でも表示できるようにした

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
+  theme: {
+    extend: {
+      screens: {
+        lg: '1096px',
+      }
+    }
+  },
   plugins: [
     require('daisyui')
   ],
