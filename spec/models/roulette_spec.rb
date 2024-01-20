214ac66f37c5e577dc20b6dc6dc08f6e9edf5afc
siso25 <siso25@zoho.com>
2024-01-21 00:03:55 +0900

    重複している処理をヘルパーメソッドに切り出し
    session storageをリセットする処理をヘルパーメソッド化した。
    また、createをメソッド名に使うとデータを作りそうに見えるので、generateを使うように変更。

--- spec/models/roulette_spec.rb
+++ spec/models/roulette_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Roulette, type: :model do
     it 'returns third color' do
       color_set = ['#6EB7DB', '#E38692', '#64C99B', '#E5D972', '#C97FB4']
       talk_theme = [FactoryBot.build(:talk_theme, roulette: @roulette)]
-      css_text = Roulette.conic_gradient_text(talk_theme, color_set)
+      css_text = Roulette.generate_background_color_text(talk_theme, color_set)
       expect(css_text).to eq 'background: conic-gradient(#64C99B 0deg 360deg);'
     end
 
@@ -21,7 +21,7 @@ RSpec.describe Roulette, type: :model do
       2.times do
         talk_themes << FactoryBot.build(:talk_theme, roulette: @roulette)
       end
-      css_text = Roulette.conic_gradient_text(talk_themes, color_set)
+      css_text = Roulette.generate_background_color_text(talk_themes, color_set)
       expect(css_text).to eq 'background: conic-gradient(#64C99B 0deg 180deg,#E5D972 180deg 360deg);'
     end
 
@@ -31,7 +31,7 @@ RSpec.describe Roulette, type: :model do
       3.times do
         talk_themes << FactoryBot.build(:talk_theme, roulette: @roulette)
       end
-      css_text = Roulette.conic_gradient_text(talk_themes, color_set)
+      css_text = Roulette.generate_background_color_text(talk_themes, color_set)
       expect(css_text).to eq 'background: conic-gradient(#6EB7DB 0deg 120deg,#E38692 120deg 240deg,#64C99B 240deg 360deg);'
     end
 
@@ -41,7 +41,7 @@ RSpec.describe Roulette, type: :model do
       4.times do
         talk_themes << FactoryBot.build(:talk_theme, roulette: @roulette)
       end
-      css_text = Roulette.conic_gradient_text(talk_themes, color_set)
+      css_text = Roulette.generate_background_color_text(talk_themes, color_set)
       expect(css_text).to eq 'background: conic-gradient(#6EB7DB 0deg 90deg,#E38692 90deg 180deg,#64C99B 180deg 270deg,#E5D972 270deg 360deg);'
     end
 
@@ -51,7 +51,7 @@ RSpec.describe Roulette, type: :model do
       5.times do
         talk_themes << FactoryBot.build(:talk_theme, roulette: @roulette)
       end
-      css_text = Roulette.conic_gradient_text(talk_themes, color_set)
+      css_text = Roulette.generate_background_color_text(talk_themes, color_set)
       expect(css_text).to eq 'background: conic-gradient(#6EB7DB 0deg 72deg,#E38692 72deg 144deg,#64C99B 144deg 216deg,#E5D972 216deg 288deg,'\
                              '#C97FB4 288deg 360deg);'
     end
@@ -62,7 +62,7 @@ RSpec.describe Roulette, type: :model do
       6.times do
         talk_themes << FactoryBot.build(:talk_theme, roulette: @roulette)
       end
-      css_text = Roulette.conic_gradient_text(talk_themes, color_set)
+      css_text = Roulette.generate_background_color_text(talk_themes, color_set)
       expect(css_text).to eq 'background: conic-gradient(#6EB7DB 0deg 60deg,#E38692 60deg 120deg,#64C99B 120deg 180deg,#E5D972 180deg 240deg,'\
                              '#C97FB4 240deg 300deg,#64C99B 300deg 360deg);'
     end
