1ca04e85d84ea00bdf9376202a8fc16dcbd8c48e
siso25 <siso25@zoho.com>
2024-01-21 00:41:55 +0900

    controllerで重複している処理をbefore_actioinにした

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -1,38 +1,39 @@
 # frozen_string_literal: true
 
 class SpeakersController < ApplicationController
+  before_action :set_roulette
+
   def new
-    @roulette = Roulette.find(params[:roulette_id])
     @speaker = Speaker.new
   end
 
   def create
-    @roulette = Roulette.find(params[:roulette_id])
     @roulette.speakers.create(speaker_params)
     @speakers = @roulette.speakers.order(:id)
     @talk_themes = @roulette.talk_themes.order(:id)
   end
 
   def edit
-    @roulette = Roulette.find(params[:roulette_id])
     @speaker = @roulette.speakers.find(params[:id])
   end
 
   def update
-    @roulette = Roulette.find(params[:roulette_id])
     @roulette.speakers.find(params[:id]).update(speaker_params)
     @speakers = @roulette.speakers.order(:id)
     @talk_themes = @roulette.talk_themes.order(:id)
   end
 
   def destroy
-    @roulette = Roulette.find(params[:roulette_id])
     @roulette.speakers.find(params[:id]).destroy
     @speakers = @roulette.speakers.order(:id)
     @talk_themes = @roulette.talk_themes.order(:id)
   end
 
   private
+
+  def set_roulette
+    @roulette = Roulette.find(params[:roulette_id])
+  end
 
   def speaker_params
     params.require(:speaker).permit(:name)
