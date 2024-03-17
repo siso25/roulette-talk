60a929719b82b40a5082e6b15f94889df2c379e1
siso25 <siso25@zoho.com>
2024-03-17 21:11:13 +0900

    ルーレットの複製成功時と失敗時の処理を追加

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -4,20 +4,14 @@ class TemplatesController < ApplicationController
   def create
     roulette = Roulette.find_by(id: params[:roulette_id])
 
-    # if roulette && roulette.talk_themes.count.positive?
-    # new_roulette = Roulette.create
-    # Template.create_talk_themes(new_roulette, roulette.talk_themes)
-    # Speaker.create_initial_records(new_roulette)
-    # redirect_to roulette_path(new_roulette)
-    @roulette = roulette
-    @talk_themes = roulette.talk_themes.order(:created_at)
-    @speakers = roulette.speakers.order(:created_at)
-    flash.now[:notice] = '新規ルーレットを作成しました！'
-    render template: 'roulettes/show'
-    # else
-    #   flash[:notice] = '新規ルーレットを作成しました！'
-    #   render roulette_path(roulette)
-    #   render 'roulettes/show'
-    # end
+    if roulette && roulette.talk_themes.count.positive?
+      new_roulette = Roulette.create
+      Template.create_talk_themes(new_roulette, roulette.talk_themes)
+      flash.notice = '新規ルーレットを作成しました。話す人を登録して使い始めましょう！'
+      redirect_to roulette_path(new_roulette)
+    else
+      flash.alert = 'ルーレットの作成に失敗しました。トークテーマは一件以上登録してください。'
+      redirect_to roulette_path(roulette)
+    end
   end
 end
--- app/javascript/controllers/toast_controller.js
+++ app/javascript/controllers/toast_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    const promise = new Promise((resolve) => setTimeout(resolve, 1500))
+    const promise = new Promise((resolve) => setTimeout(resolve, 3000))
     promise.then(() => this.fadeOut(this.element))
   }
 
@@ -14,7 +14,7 @@ export default class extends Controller {
       { visibility: 'visible', opacity: '0.5' },
       { visibility: 'hidden', opacity: '0' }
     ]
-    await element.animate(keyframes, 200)
+    await element.animate(keyframes, 400)
     element.classList.add('invisible')
   }
 }
