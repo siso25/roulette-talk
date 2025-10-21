58bf9e4a7d92ef3a96fc284125b2a89a5a3366c4
siso25 <siso25@zoho.com>
2025-10-21 22:34:56 +0900

    フラッシュメッセージコンポーネントのリファクタリング

--- app/components/toast/component.rb
+++ app/components/toast/component.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 
 class Toast::Component < ViewComponent::Base
-  def initialize(flash:)
-    @flash = flash
+  def initialize(type:, message:)
+    @type = type
+    @message = message
+  end
+
+  private
+
+  def color_class
+    case @type
+    when 'notice'
+      'alert-success'
+    when 'alert'
+      'alert-error'
+    else
+      ''
+    end
   end
 end
